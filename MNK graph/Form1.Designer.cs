2d6c792b8ff1fb69c4514fb6f66ddc8711fb96db
danil <danil@DESKTOP-JI6M9JU>
2022-02-18 13:06:19 +0200

    Добавьте файлы проекта.

---
+++ MNK graph/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace MNK_graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.GroupBox FilesInBox;
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yList = new System.Windows.Forms.TextBox();
+            this.xList = new System.Windows.Forms.TextBox();
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.GraphBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TestTextOut = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.import = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            FilesInBox = new System.Windows.Forms.GroupBox();
+            FilesInBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.GraphBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FilesInBox
+            // 
+            FilesInBox.Controls.Add(this.label2);
+            FilesInBox.Controls.Add(this.label1);
+            FilesInBox.Controls.Add(this.yList);
+            FilesInBox.Controls.Add(this.xList);
+            FilesInBox.Location = new System.Drawing.Point(474, 12);
+            FilesInBox.Name = "FilesInBox";
+            FilesInBox.Size = new System.Drawing.Size(296, 99);
+            FilesInBox.TabIndex = 4;
+            FilesInBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // yList
+            // 
+            this.yList.Location = new System.Drawing.Point(23, 58);
+            this.yList.Name = "yList";
+            this.yList.Size = new System.Drawing.Size(222, 20);
+            this.yList.TabIndex = 1;
+            // 
+            // xList
+            // 
+            this.xList.Location = new System.Drawing.Point(23, 20);
+            this.xList.Name = "xList";
+            this.xList.Size = new System.Drawing.Size(222, 20);
+            this.xList.TabIndex = 0;
+            this.xList.TextChanged += new System.EventHandler(this.xList_TextChanged);
+            // 
+            // graph
+            // 
+            this.graph.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            chartArea2.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            legend2.Title = "Позначення:";
+            legend2.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend2.TitleForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.graph.Legends.Add(legend2);
+            this.graph.Location = new System.Drawing.Point(6, 10);
+            this.graph.Name = "graph";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.Name = "Точки";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "ax+b";
+            this.graph.Series.Add(series3);
+            this.graph.Series.Add(series4);
+            this.graph.Size = new System.Drawing.Size(429, 410);
+            this.graph.TabIndex = 1;
+            this.graph.Text = "chart1";
+            title2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title2.Name = "Title1";
+            title2.Text = "Графік МНК";
+            this.graph.Titles.Add(title2);
+            this.graph.Click += new System.EventHandler(this.graph_1_Click);
+            // 
+            // GraphBox
+            // 
+            this.GraphBox.Controls.Add(this.graph);
+            this.GraphBox.Location = new System.Drawing.Point(21, 12);
+            this.GraphBox.Name = "GraphBox";
+            this.GraphBox.Size = new System.Drawing.Size(441, 426);
+            this.GraphBox.TabIndex = 2;
+            this.GraphBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Побудувати графік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // TestTextOut
+            // 
+            this.TestTextOut.Location = new System.Drawing.Point(474, 395);
+            this.TestTextOut.Name = "TestTextOut";
+            this.TestTextOut.Size = new System.Drawing.Size(290, 20);
+            this.TestTextOut.TabIndex = 6;
+            this.TestTextOut.TextChanged += new System.EventHandler(this.TestTextOut_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ClearBtn);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(468, 301);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 64);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ClearBtn.ForeColor = System.Drawing.Color.Black;
+            this.ClearBtn.Location = new System.Drawing.Point(215, 13);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 44);
+            this.ClearBtn.TabIndex = 6;
+            this.ClearBtn.Text = "Очистити";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            this.ClearBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // import
+            // 
+            this.import.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.import.Location = new System.Drawing.Point(474, 117);
+            this.import.Name = "import";
+            this.import.Size = new System.Drawing.Size(75, 24);
+            this.import.TabIndex = 1;
+            this.import.Text = "Імпорт txt";
+            this.import.UseMnemonic = false;
+            this.import.UseVisualStyleBackColor = true;
+            this.import.Click += new System.EventHandler(this.import_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(471, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Коефіцієнти:";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.import);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TestTextOut);
+            this.Controls.Add(FilesInBox);
+            this.Controls.Add(this.GraphBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            FilesInBox.ResumeLayout(false);
+            FilesInBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.GraphBox.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
+        private System.Windows.Forms.GroupBox GraphBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox yList;
+        private System.Windows.Forms.TextBox xList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TestTextOut;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.Button import;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MNK graph/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace MNK_graph
+{
+    public partial class Form1 : Form
+    {
+        private string x;
+        private string y;
+        private bool checker;
+        private bool checker1 = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            graph.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            graph.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void graph_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xList_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(xList.Text))
+            {
+                errorProvider1.SetError(xList, "Укажите координаты");
+                this.checker1 = true;
+                goto ERROR1;
+            }
+
+            this.x = xList.Text.Trim();
+            this.y = yList.Text.Trim();
+            string[] delim = { " ", "  " };
+
+            var x1Arr = this.x.Split(' ').Select(Double.Parse).ToArray();
+            var y1Arr = this.y.Split(' ').Select(Double.Parse).ToArray();
+
+            if (x1Arr.Length != y1Arr.Length)
+            {
+                this.checker = true;
+            }
+
+            double[] xArr = new double[Math.Min(x1Arr.Length, y1Arr.Length)];
+            double[] yArr = new double[Math.Min(x1Arr.Length, y1Arr.Length)];
+            CoordValid();
+
+            graph.Series[0].Points.Clear();
+            graph.Series[1].Points.Clear();
+
+
+            if (x1Arr.Length > y1Arr.Length)
+            {
+                Array.Copy(x1Arr, 0, xArr, 0, Math.Min(x1Arr.Length, y1Arr.Length));
+                yArr = y1Arr;
+                this.checker = true;
+            }
+
+            else if (x1Arr.Length < y1Arr.Length)
+            {
+                Array.Copy(y1Arr, 0, yArr, 0, Math.Min(x1Arr.Length, y1Arr.Length));
+                xArr = x1Arr;
+                this.checker = true;
+            }
+            else
+            {
+                xArr = x1Arr;
+                yArr = y1Arr;
+                this.checker = false;
+            }
+
+            for (int i = 0; i < Math.Min(x1Arr.Length, y1Arr.Length); i++)
+            {
+                graph.Series[0].Points.AddXY(xArr[i], yArr[i]);
+            }
+            double a, b;
+
+            Approx(xArr, yArr, out a, out b);
+
+            TestTextOut.Text = $"a = {a:F3}, b = {b:F3}";
+            graph.Series[1].Points.AddXY((xArr[0]), (xArr[0] * a + b));
+            graph.Series[1].Points.AddXY((xArr[xArr.Length - 1]), (xArr[xArr.Length - 1] * a + b));
+            CoordValid();
+            ERROR1:;
+
+
+
+        }
+        private void Approx(double[] x, double[] y, out double a, out double b)
+        {
+            int n = x.Length;
+            double[] xy = new double[n];
+            double[] x2 = new double[n];
+            double xSum,ySum, xySum, x2Sum;
+            xSum = ySum = xySum = x2Sum = 0;
+            
+            for (int i = 0; i < n; i++) xSum += x[i];
+            for (int i = 0; i < n; i++) ySum += y[i];
+            for (int i = 0; i < n; i++) xySum += y[i]*x[i];
+            for (int i = 0; i < n; i++) x2Sum += x[i]*x[i];
+           
+
+            a = (xSum * ySum - n * xySum) / ((xSum * xSum) - (n * x2Sum));
+            b = (xSum * xySum - x2Sum * ySum) / (xSum * xSum - n * x2Sum);
+        }
+
+        private void TestTextOut_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            graph.Series[0].Points.Clear();
+            graph.Series[1].Points.Clear();
+            xList.Text = "";
+            yList.Text = "";
+        }
+
+        private void import_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    filePath = openFileDialog.FileName;                    
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            char[] delim = { ' ', '\t', '\n', '\r' };
+            string [] arr = fileContent.Split(delim);
+            var n = arr.Length / 2;
+            string[] xarr = new string[n];
+            string[] yarr = new string[n];
+            Array.Copy(arr, xarr, n);
+            Array.Copy(arr, n+1, yarr, 0, n);
+
+            xList.Text = String.Join(" ", xarr);
+            yList.Text = String.Join(" ", yarr);
+        }
+        private void CoordValid()
+        {
+            if (String.IsNullOrEmpty(xList.Text))
+            {
+                errorProvider1.SetError(xList, "Укажите координаты");
+                this.checker1 = true;
+            }
+            else if (this.checker)
+            {
+                errorProvider1.SetError(xList, "Кол-во координат не совпадает");
+                this.checker1 = true;
+            }
+            else if (String.IsNullOrEmpty(yList.Text))
+            {
+                errorProvider1.SetError(yList, "Укажите координаты");
+            }
+            else
+            {
+                errorProvider1.Clear();
+                this.checker1 = false;
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
